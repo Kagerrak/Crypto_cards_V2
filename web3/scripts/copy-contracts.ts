7a8fcd8460ecfd5b70e90ccc400775916fab1e7f
Kage <romzhey@gmail.com>
2023-03-31 08:38:18 +0800

    minor update

--- web3/scripts/copy-contracts.ts
+++ web3/scripts/copy-contracts.ts
@@ -1,7 +1,12 @@
 import fs from "fs";
 import path from "path";
 
-async function copyContracts() {
+async function copyContracts(
+  battleAddress: string,
+  characterAddress: string,
+  battleSkillsAddress: string,
+  battleItemsAddress: string
+) {
   const outputDir = path.join(
     __dirname,
     "..",
@@ -49,6 +54,7 @@ async function copyContracts() {
   const outputPathBattleItems = path.join(outputDir, "battleItems.json");
   const outputPathBattleSkills = path.join(outputDir, "battleSkills.json");
   const outputPathCharacter = path.join(outputDir, "characterContract.json");
+  const outputPathIndex = path.join(outputDir, "index.js");
 
   fs.copyFileSync(artifactPathBattle, outputPathBattle);
   console.log(`Copied Battle contract data to ${outputPathBattle}!`);
@@ -63,6 +69,28 @@ async function copyContracts() {
 
   fs.copyFileSync(artifactPathCharacter, outputPathCharacter);
   console.log(`Copied Character contract data to ${outputPathCharacter}!`);
+
+  const indexFileContent = `
+  import characterContract from "./characterContract.json";
+  import battleSkills from "./battleSkills.json";
+  import battleItems from "./battleItems.json";
+  import battleContract from "./battleContract.json";
+  
+  export const characterContractAddress = "${characterAddress}";
+  export const characterContractABI = characterContract.abi;
+  
+  export const battleSkillsAddress = "${battleSkillsAddress}";
+  export const battleSkillsABI = battleSkills.abi;
+  
+  export const battleItemsAddress = "${battleItemsAddress}";
+  export const battleItemsABI = battleItems.abi;
+  
+  export const battleContractAddress = "${battleAddress}";
+  export const battleContractABI = battleContract.abi;
+  `;
+
+  fs.writeFileSync(outputPathIndex, indexFileContent);
+  console.log(`Wrote contract addresses to ${outputPathIndex}!`);
 }
 
 export default copyContracts;
