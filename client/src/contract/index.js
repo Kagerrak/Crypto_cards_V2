851ffc2094a74cf5f1dad127acacdec90267a9b5
Kage <romzhey@gmail.com>
2023-04-01 23:52:19 +0800

    new contracts

--- client/src/contract/index.js
+++ client/src/contract/index.js
@@ -1,18 +1,18 @@
-import characterContract from "./characterContract.json";
-import battleSkills from "./battleSkills.json";
-import battleItems from "./battleItems.json";
-import battleContract from "./battleContract.json";
 
-export const characterContractAddress =
-  "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
-export const characterContractABI = characterContract.abi;
-
-export const battleSkillsAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const battleSkillsABI = battleSkills.abi;
-
-export const battleItemsAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-export const battleItemsABI = battleItems.abi;
-
-export const battleContractAddress =
-  "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
-export const battleContractABI = battleContract.abi;
+  import characterContract from "./characterContract.json";
+  import battleSkills from "./battleSkills.json";
+  import battleItems from "./battleItems.json";
+  import battleContract from "./battleContract.json";
+  
+  export const characterContractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+  export const characterContractABI = characterContract.abi;
+  
+  export const battleSkillsAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  export const battleSkillsABI = battleSkills.abi;
+  
+  export const battleItemsAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+  export const battleItemsABI = battleItems.abi;
+  
+  export const battleContractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+  export const battleContractABI = battleContract.abi;
+  
