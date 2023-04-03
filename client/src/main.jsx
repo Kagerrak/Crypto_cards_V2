5de382f82e4c2bbf2ba2f5746fe38ce290ab51d6
Kage <romzhey@gmail.com>
2023-04-03 23:01:09 +0800

    added thirdwebProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import {
@@ -25,26 +26,28 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <GlobalContextProvider>
-      <Navbar />
-      <OnboardModal />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/battleground" element={<Battleground />} />
-        <Route path="/battle/:battleName" element={<Battle />} />
-        <Route path="/create-battle" element={<CreateBattle />} />
-        <Route path="/join-battle" element={<JoinBattle />} />
-        <Route path="/colosseum" element={<Colosseum />} />
-        <Route path="/recruitment-guild" element={<RecruitmentGuild />} />
-        <Route path="/my-champions" element={<MyChampion />} />
-        <Route path="/training-guild" element={<TrainingGuild />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/weapon-armor-shop" element={<ItemShop />} />
-        <Route path="/item-shop" element={<WeaponShop />} />
-        <Route path="/magic-trinket-shop" element={<MagicShop />} />
-        <Route path="/inn" element={<InnPage />} />
-        <Route path="/withdraw" element={<Withdraw />} />
-      </Routes>
-    </GlobalContextProvider>
+    <ThirdwebProvider activeChain="localhost">
+      <GlobalContextProvider>
+        <Navbar />
+        <OnboardModal />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/battleground" element={<Battleground />} />
+          <Route path="/battle/:battleName" element={<Battle />} />
+          <Route path="/create-battle" element={<CreateBattle />} />
+          <Route path="/join-battle" element={<JoinBattle />} />
+          <Route path="/colosseum" element={<Colosseum />} />
+          <Route path="/recruitment-guild" element={<RecruitmentGuild />} />
+          <Route path="/my-champions" element={<MyChampion />} />
+          <Route path="/training-guild" element={<TrainingGuild />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/weapon-armor-shop" element={<ItemShop />} />
+          <Route path="/item-shop" element={<WeaponShop />} />
+          <Route path="/magic-trinket-shop" element={<MagicShop />} />
+          <Route path="/inn" element={<InnPage />} />
+          <Route path="/withdraw" element={<Withdraw />} />
+        </Routes>
+      </GlobalContextProvider>
+    </ThirdwebProvider>
   </BrowserRouter>
 );
