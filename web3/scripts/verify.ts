90dca781690eaffd75429fda2aa69cc5c80a1f5f
Kage <romzhey@gmail.com>
2023-04-04 12:12:19 +0800

    first commit

---
+++ web3/scripts/verify.ts
@@ -0,0 +1,21 @@
+import { run } from "hardhat";
+
+const verify = async (contractAddress: string, args: any[]) => {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e: unknown) {
+    if (typeof e === "object" && e !== null && "message" in e) {
+      if ((e as Error).message.toLowerCase().includes("already verified")) {
+        console.log("Already verified!");
+      } else {
+        console.log((e as Error).message);
+      }
+    }
+  }
+};
+
+export { verify };
