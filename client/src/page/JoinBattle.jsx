7f40d51f34f965d176469a9784837419fbe76356
Kage <romzhey@gmail.com>
2023-04-09 20:16:17 +0800

    minor update

--- client/src/page/JoinBattle.jsx
+++ client/src/page/JoinBattle.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  useOwnedNFTs,
+  useContract,
+  ThirdwebNftMedia,
+} from "@thirdweb-dev/react";
 
+import { characterContractAddress } from "../contract";
 import { useGlobalContext } from "../context";
 import { CustomButton, PageHOC, Loader } from "../components";
 import styles from "../styles";
 
 const JoinBattle = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [selectedTokenID, setSelectedTokenID] = useState(null);
   const navigate = useNavigate();
   const {
-    contract,
+    battleContract,
     gameData,
     setShowAlert,
     setBattleName,
@@ -17,22 +24,28 @@ const JoinBattle = () => {
     walletAddress,
   } = useGlobalContext();
 
+  const { contract: charContract, isLoading: charLoad } = useContract(
+    characterContractAddress
+  );
+
+  const { data: ownedNfts } = useOwnedNFTs(charContract, walletAddress);
+
   useEffect(() => {
     if (gameData?.activeBattle?.battleStatus === 1) {
       navigate(`/battle/${gameData.activeBattle.name}`);
     }
   }, [gameData]);
 
-  const handleClick = async (battleName) => {
+  const handleClick = async (battleName, battleId, characterId) => {
     setBattleName(battleName);
+    console.log(battleId.toNumber(), characterId);
 
     try {
       setIsLoading(true);
-      const fees = await contract.battleFee();
-      const tx = await contract.joinBattle(battleName, {
-        gasLimit: 500000,
-        value: fees,
-      });
+      const tx = await battleContract.joinBattle(
+        battleId.toNumber(),
+        characterId
+      );
       await tx.wait();
       setIsLoading(false);
 
@@ -47,10 +60,41 @@ const JoinBattle = () => {
     }
   };
 
+  let content;
+  if (charLoad) {
+    content = <Loader />;
+  } else if (Array.isArray(ownedNfts) && ownedNfts.length > 0) {
+    console.log(ownedNfts);
+    content = ownedNfts.map((c) => (
+      <div
+        key={c.metadata.id}
+        className={`${styles.nftContainer} hover:${
+          styles.nftOverlayHover
+        } focus:${styles.nftOverlayHover} ${
+          selectedTokenID === c.metadata.id ? styles.nftSelected : ""
+        }`}
+        onClick={() => setSelectedTokenID(c.metadata.id)}
+      >
+        {console.log(selectedTokenID)}
+        <div className={styles.nftOverlay} />
+        <ThirdwebNftMedia metadata={c.metadata} height={200} />
+      </div>
+    ));
+  } else {
+    content = (
+      <div className="text-center">
+        <p>You don't have a character!</p>
+        <p>Create one and join the battle!</p>
+      </div>
+    );
+  }
+
   return (
     <>
       {isLoading && <Loader />}
       <h2 className={styles.joinHeadText}>Available Battles:</h2>
+
+      {content}
 
       <div className={styles.joinContainer}>
         {gameData.pendingBattles.length ? (
@@ -61,13 +105,15 @@ const JoinBattle = () => {
                 battle.battleStatus !== 1
             )
             .map((battle, index) => (
-              <div key={battle.name + index} className={styles.flexBetween}>
+              <div key={battle.battleId + index} className={styles.flexBetween}>
                 <p className={styles.joinBattleTitle}>
-                  {index + 1}. {battle.name}
+                  {index + 1}. {battle.name} {battle.battleId.toNumber()}
                 </p>
                 <CustomButton
                   title="Join"
-                  handleClick={() => handleClick(battle.name)}
+                  handleClick={() =>
+                    handleClick(battle.name, battle.battleId, selectedTokenID)
+                  }
                 />
               </div>
             ))
