f3414ae24acc171dc1750a191a7b231cf9bc464d
Kage <romzhey@gmail.com>
2023-04-13 23:05:58 +0800

    bug fix

--- client/src/page/Battle.jsx
+++ client/src/page/Battle.jsx
@@ -1,9 +1,16 @@
 /* eslint-disable prefer-destructuring */
-import React, { useEffect, useState, useLayoutEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import styles from "../styles";
-import { ActionButton, Alert, Card, GameInfo, PlayerInfo } from "../components";
+import {
+  ActionButton,
+  Alert,
+  Card,
+  GameInfo,
+  PlayerInfo,
+  Loader,
+} from "../components";
 import { useGlobalContext } from "../context";
 import {
   attack,
@@ -27,132 +34,95 @@ const Battle = () => {
     setShowAlert,
     player1Ref,
     player2Ref,
+    playerData,
   } = useGlobalContext();
 
-  const [player2, setPlayer2] = useState({});
-  const [player1, setPlayer1] = useState({});
-  const [playersLoaded, setPlayersLoaded] = useState(false);
-  const [character, setCharacter] = useState();
-  const [p1InitHP, setP1InitHP] = useState(null);
-  const [p2InitHP, setP2InitHP] = useState(null);
-  const [battleId, setBattleId] = useState(null);
+  const [state, setState] = useState({
+    player1: {},
+    player2: {},
+    playersLoaded: false,
+    character: null,
+    p1InitHP: null,
+    p2InitHP: null,
+    battleId: null,
+  });
+
   const [loading, setLoading] = useState(true);
-  const [fetched, setFetched] = useState(false);
   const navigate = useNavigate();
 
-  useLayoutEffect(() => {
-    const getPlayerInfo = async () => {
-      try {
-        setLoading(true);
-        let player01Address = null;
-        let player02Address = null;
+  useEffect(() => {
+    if (playerData.player1Data && playerData.player2Data) {
+      if (!gameData.activeBattle || !gameData.activeBattle.initialHealth) {
+        return;
+      }
+      const player01 = playerData.player1Data;
+      const player02 = playerData.player2Data;
+      const typeId = player01.id.toNumber();
+      const characterInfo = characters.filter(
+        (item) => item.characterType === typeId
+      )[0];
 
-        if (
-          gameData.activeBattle.players[0].toLowerCase() ===
-          walletAddress.toLowerCase()
-        ) {
-          player01Address = gameData.activeBattle.players[0];
-          player02Address = gameData.activeBattle.players[1];
-          setP1InitHP(gameData.activeBattle.initialHealth[0].toNumber());
-          setP2InitHP(gameData.activeBattle.initialHealth[1].toNumber());
-          setBattleId(gameData.activeBattle.battleId);
-        } else {
-          player01Address = gameData.activeBattle.players[1];
-          player02Address = gameData.activeBattle.players[0];
-          setP1InitHP(gameData.activeBattle.initialHealth[1].toNumber());
-          setP2InitHP(gameData.activeBattle.initialHealth[0].toNumber());
-          setBattleId(gameData.activeBattle.battleId);
-        }
-
-        console.log(p1InitHP, p2InitHP);
-
-        const [player01, player02] = await Promise.all([
-          battleContract.getCharacterProxy(battleId, player01Address),
-          battleContract.getCharacterProxy(battleId, player02Address),
-        ]);
-
-        console.log(player01);
-
-        const p1Att = player01.attack.toNumber();
-        console.log(p1Att);
-        const p1Def = player01.defense.toNumber();
-
-        const typeId = player01.id.toNumber();
-        console.log(typeId);
-
-        const characterInfo = characters.filter(
-          (item) => item.characterType === typeId
-        )[0];
-        console.log(characterInfo);
-
-        if (characterInfo) {
-          setCharacter(characterInfo);
-        } else {
-          setCharacter(null);
-        }
-
-        const p1H = player01.health.toNumber();
-        console.log(p1H);
-        const p1M = player01.mana.toNumber();
-        const p2H = player02.health.toNumber();
-        const p2M = player02.mana.toNumber();
-
-        setPlayer1({
-          att: p1Att,
-          def: p1Def,
-          health: p1H,
-          mana: p1M,
-        });
-        setPlayer2({
+      const newState = {
+        player1: {
+          att: player01.attack.toNumber(),
+          def: player01.defense.toNumber(),
+          health: player01.health.toNumber(),
+          mana: player01.mana.toNumber(),
+        },
+        player2: {
           att: "X",
           def: "X",
-          health: p2H,
-          mana: p2M,
-        });
-        setPlayersLoaded(true);
-      } catch (error) {
-        setErrorMessage(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+          health: player02.health.toNumber(),
+          mana: player02.mana.toNumber(),
+        },
+        p1InitHP:
+          gameData.activeBattle.initialHealth[
+            gameData.activeBattle.players[0].toLowerCase() ===
+            walletAddress.toLowerCase()
+              ? 0
+              : 1
+          ].toNumber(),
+        p2InitHP:
+          gameData.activeBattle.initialHealth[
+            gameData.activeBattle.players[0].toLowerCase() ===
+            walletAddress.toLowerCase()
+              ? 1
+              : 0
+          ].toNumber(),
+        battleId: gameData.activeBattle.battleId,
+        character: characterInfo || null,
+      };
 
-    if (battleContract && gameData.activeBattle && !playersLoaded && !fetched) {
-      getPlayerInfo();
-    } else {
+      setState(newState);
+    }
+  }, [playerData, gameData, walletAddress]);
+
+  useEffect(() => {
+    if (
+      state.player1 &&
+      state.player2 &&
+      state.p1InitHP !== null &&
+      state.p2InitHP !== null &&
+      state.battleId !== null &&
+      state.character
+    ) {
       setLoading(false);
     }
-  }, [
-    battleContract,
-    gameData.activeBattle,
-    walletAddress,
-    character,
-    playersLoaded,
-    fetched,
-  ]);
-
-  console.log(player1.att);
+  }, [state]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      if (!gameData?.activeBattle) navigate("/");
-    }, [2000]);
+      if (!gameData?.activeBattle) navigate("/create-battle");
+    }, [3000]);
 
     return () => clearTimeout(timer);
-  }, [battleContract]);
-
-  useEffect(() => {
-    if (playersLoaded) {
-      setFetched(true);
-      setLoading(false);
-    }
-  }, [playersLoaded]);
+  }, [battleContract, gameData, gameData.activeBattle]);
 
   const makeAMove = async (choice) => {
     playAudio(choice === 0 ? attackSound : choice === 1 ? defenseSound : null);
 
     try {
-      await battleContract.submitMove(battleId, choice, {
+      await battleContract.submitMove(state.battleId, choice, {
         gasLimit: 200000,
       });
 
@@ -167,40 +137,37 @@ const Battle = () => {
       setErrorMessage(error);
     }
   };
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div
       className={`${styles.flexBetween} ${styles.gameContainer} ${battleGround}`}
     >
-      {showAlert?.status && (
-        <Alert type={showAlert.type} message={showAlert.message} />
-      )}
-
-      {fetched && (
+      {loading ? (
+        <Loader />
+      ) : (
         <>
+          {showAlert?.status && (
+            <Alert type={showAlert.type} message={showAlert.message} />
+          )}
+
           <PlayerInfo
-            player={player2}
+            player={state.player2}
             playerIcon={player02Icon}
             mt
-            health={p2InitHP}
+            health={state.p2InitHP}
           />
 
           <div className={`${styles.flexCenter} flex-col my-10`}>
             <Card
-              card={player2}
-              title={player2?.id}
+              card={state.player2}
+              title={state.player2?.id}
               cardRef={player2Ref}
               playerTwo
             />
 
-            {character && (
+            {state.character && (
               <div className="flex items-center flex-row">
                 <ActionButton
-                  imgUrl={character.icon}
+                  imgUrl={state.character.icon}
                   handleClick={() => makeAMove(2)}
                   restStyles="ml-6 mt-6 hover:border-red-600"
                 />
@@ -215,8 +182,8 @@ const Battle = () => {
               />
 
               <Card
-                card={player1}
-                title={player1.id}
+                card={state.player1}
+                title={state.player1.id}
                 cardRef={player1Ref}
                 restStyles="mt-3"
               />
@@ -230,16 +197,16 @@ const Battle = () => {
           </div>
 
           <PlayerInfo
-            player={player1}
+            player={state.player1}
             playerIcon={player01Icon}
-            health={p1InitHP}
+            health={state.p1InitHP}
           />
+
+          <GameInfo id={state.battleId} />
         </>
       )}
-
-      <GameInfo id={battleId} />
     </div>
   );
 };
 
-export default Battle;
+export default React.memo(Battle);
