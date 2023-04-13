fadada6c1b62d4dc0601288e3ada354100329fec
Kage <romzhey@gmail.com>
2023-04-13 23:04:18 +0800

    mana update

--- client/src/components/PlayerInfo.jsx
+++ client/src/components/PlayerInfo.jsx
@@ -16,88 +16,118 @@ import emptyHealthbar from "../assets/empty_healthbar.png";
 // const marginIndexing = (index) =>
 //   index !== healthPoints - 1 ? "mr-1" : "mr-0";
 
-const PlayerInfo = ({ player, playerIcon, character, mt, health }) => (
-  <div className={`${styles.flexCenter} ${mt ? "mt-auto" : "mb-auto"}`}>
-    <img
-      data-for={`Player-${mt ? "1" : "2"}`}
-      data-tip
-      src={playerIcon}
-      alt="player02"
-      className="w-14 h-14 object-contain rounded-full"
-    />
+const PlayerInfo = ({ player, playerIcon, character, mt, health }) => {
+  function getManaPercentage(mana, maxMana) {
+    const percent = (mana / maxMana) * 100;
+    const circumference = 2 * Math.PI * 50;
+    const offset = circumference - (percent / 100) * circumference;
+    return { dasharray: circumference, dashoffset: offset };
+  }
+  return (
+    <div className={`${styles.flexCenter} ${mt ? "mt-auto" : "mb-auto"}`}>
+      <img
+        data-for={`Player-${mt ? "1" : "2"}`}
+        data-tip
+        src={playerIcon}
+        alt="player02"
+        className="w-14 h-14 object-contain rounded-full"
+      />
 
-    <div
-      data-for={`Health-${mt ? "1" : "2"}`}
-      data-tip={`Health: ${player.health}`}
-      className={styles.playerHealth}
-    >
       <div
-        style={{
-          width: "200%",
-          height: "100%",
-          position: "relative",
-          marginBottom: "30px",
-        }}
+        data-for={`Health-${mt ? "1" : "2"}`}
+        data-tip={`Health: ${player.health}`}
+        className={styles.playerHealth}
       >
-        <img
-          src={`${emptyHealthbar}`}
-          style={{ width: "100%", height: "140%", position: "absolute" }}
-          className="rounded-[10px]"
-        />
-        <img
-          src={`${fullHealthbar}`}
+        <div
           style={{
-            width: `${(player.health / health) * 100}%`,
-            height: "115%",
-            position: "absolute",
-            paddingLeft: "8px",
-            paddingTop: "2px",
+            width: "200%",
+            height: "100%",
+            position: "relative",
+            marginBottom: "30px",
           }}
-          className="rounded-l-[10px]"
-        />
+        >
+          <img
+            src={`${emptyHealthbar}`}
+            style={{ width: "100%", height: "140%", position: "absolute" }}
+            className="rounded-[10px]"
+          />
+          <img
+            src={`${fullHealthbar}`}
+            style={{
+              width: `${(player.health / health) * 100}%`,
+              height: "115%",
+              position: "absolute",
+              paddingLeft: "8px",
+              paddingTop: "2px",
+            }}
+            className="rounded-l-[10px]"
+          />
+        </div>
       </div>
-    </div>
 
-    <div
-      data-for={`Mana-${mt ? "1" : "2"}`}
-      data-tip="Mana"
-      className={`${styles.flexCenter} ${styles.glassEffect} ${styles.playerMana}`}
-    >
-      <div className={styles.playerManaContainer}>
-        {player.mana === 0 ? (
-          <img src={emptyMana} alt="Logo" />
-        ) : (
-          <img src={fullMana} alt="Logo" />
-        )}
-        <h1 className={styles.playerManaText}> {player.mana || 0}</h1>
+      <div
+        data-for={`Mana-${mt ? "1" : "2"}`}
+        data-tip="Mana"
+        className={`${styles.flexCenter} ${styles.glassEffect} ${styles.playerMana}`}
+      >
+        <div className={styles.playerManaContainer}>
+          <div className={styles.circularProgress}>
+            <svg width="100" height="100" viewBox="0 0 120 120">
+              <circle
+                cx="60"
+                cy="60"
+                r="50"
+                stroke="white"
+                strokeWidth="20"
+                fill="none"
+              />
+              <circle
+                cx="60"
+                cy="60"
+                r="50"
+                stroke="blue"
+                strokeWidth="20"
+                strokeLinecap="round"
+                fill="none"
+                strokeDasharray={getManaPercentage(player.mana, 100).dasharray}
+                strokeDashoffset={
+                  getManaPercentage(player.mana, 100).dashoffset
+                }
+                transform="rotate(-90 60 60)"
+              />
+            </svg>
+            <img src={emptyMana} alt="Logo" style={{ position: "absolute" }} />
+          </div>
+          <h1 className={styles.playerManaText}> {player.mana || 0}</h1>
+        </div>
       </div>
-    </div>
 
-    <ReactTooltip
-      id={`Player-${mt ? "1" : "2"}`}
-      effect="solid"
-      backgroundColor="#7f46f0"
-    >
-      <p className={styles.playerInfo}>
-        <span className={styles.playerInfoSpan}>Name:</span>{" "}
-        {player?.playerName}
-      </p>
-      <p className={styles.playerInfo}>
-        <span className={styles.playerInfoSpan}>Address:</span>{" "}
-        {player?.playerAddress?.slice(0, 10)}
-      </p>
-    </ReactTooltip>
-    <ReactTooltip
-      id={`Health-${mt ? "1" : "2"}`}
-      effect="solid"
-      backgroundColor="#7f46f0"
-    />
-    <ReactTooltip
-      id={`Mana-${mt ? "1" : "2"}`}
-      effect="solid"
-      backgroundColor="#7f46f0"
-    />
-  </div>
-);
+      <ReactTooltip
+        id={`Player-${mt ? "1" : "2"}`}
+        effect="solid"
+        backgroundColor="#7f46f0"
+      >
+        <p className={styles.playerInfo}>
+          <span className={styles.playerInfoSpan}>Name:</span>{" "}
+          {player?.playerName}
+        </p>
+        <p className={styles.playerInfo}>
+          <span className={styles.playerInfoSpan}>Address:</span>{" "}
+          {player?.playerAddress?.slice(0, 10)}
+        </p>
+      </ReactTooltip>
+      <ReactTooltip
+        id={`Health-${mt ? "1" : "2"}`}
+        effect="solid"
+        backgroundColor="#7f46f0"
+      />
+      <ReactTooltip
+        id={`Mana-${mt ? "1" : "2"}`}
+        effect="solid"
+        backgroundColor="#7f46f0"
+      />
+    </div>
+  );
+};
 
 export default PlayerInfo;
