3eff1e47cc4b5a2bd788a1f28c3fc7f58318b6a0
Kage <romzhey@gmail.com>
2023-04-19 23:23:52 +0800

    minor changes

--- client/src/components/StatInput.jsx
+++ client/src/components/StatInput.jsx
@@ -6,6 +6,7 @@ const StatInput = ({
   statPoints,
   setStatPoints,
   setStatChanges,
+  onStatChange, // Add this prop
   showButtons = true,
 }) => {
   const [input, setInput] = useState(stat);
@@ -19,25 +20,28 @@ const StatInput = ({
       const newInput = input - 1;
       setInput(newInput);
       setStatPoints((prevStatPoints) => prevStatPoints + (input - newInput));
-      setStatChanges((prevStatChanges) => prevStatChanges + (input - newInput));
+      //   setStatChanges((prevStatChanges) => prevStatChanges + (input - newInput));
+      onStatChange(name, newInput); // Call the onStatChange prop here
     }
   };
 
   const handleIncrement = () => {
     if (statPoints > 0) {
-      setInput(input + 1);
+      const newInput = input + 1;
+      setInput(newInput);
       setStatPoints((prevStatPoints) => prevStatPoints - 1);
-      setStatChanges((prevStatChanges) => prevStatChanges + 1);
+      //   setStatChanges((prevStatChanges) => prevStatChanges + 1);
+      onStatChange(name, newInput); // Call the onStatChange prop here
     }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center mt-1">
+    <div className="w-[129px] h-[80px] flex flex-col items-center justify-center">
       <div>
         <label className="mx-2">{name}</label>
       </div>
       {showButtons && (
-        <div className="flex flex-row">
+        <div className="flex flex-row space-x-1">
           <button
             onClick={handleDecrement}
             className="w-[30px] h-[30px] bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full flex items-center justify-center"
