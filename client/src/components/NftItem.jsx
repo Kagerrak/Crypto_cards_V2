2f4396abce5ba32e3ec5e396975181787352a272
Kage <romzhey@gmail.com>
2023-05-09 23:06:04 +0800

    minor changes

--- client/src/components/NftItem.jsx
+++ client/src/components/NftItem.jsx
@@ -5,13 +5,14 @@ const ProgressBar = ({ value, max, color }) => {
   const percentage = (value / max) * 100;
 
   return (
-    <div className="w-full h-2 bg-gray-300 rounded mb-1">
+    <div className="w-full h-2 bg-gray-300 rounded mb-1 relative">
       <div
-        className={`h-2 ${color} rounded flex items-center justify-center`}
+        className={`h-full ${color} rounded`}
         style={{ width: `${percentage}%` }}
-      >
-        <span className="text-[12px] text-gray-800 font-bold">{value}</span>
-      </div>
+      />
+      <span className="text-[12px] text-gray-800 font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        {value}
+      </span>
     </div>
   );
 };
@@ -19,20 +20,32 @@ const ProgressBar = ({ value, max, color }) => {
 const NftItem = ({ metadata, isSelected, onSelect, contract }) => {
   const [charMana, setCharMana] = useState(null);
   const [charStamina, setCharStamina] = useState(null);
+  const [charExperience, setCharExperience] = useState(null);
+  const [charLevel, setCharLevel] = useState(null);
+  const [maxExperience, setMaxExperience] = useState(null);
 
   useEffect(() => {
-    const fetchMana = async () => {
+    const fetchStats = async () => {
       try {
         const mana = await contract.call("getMana", [metadata.id]);
         const stamina = await contract.call("getStamina", [metadata.id]);
+        const character = await contract.call("getCharacter", [metadata.id]);
+        const experience = character.experience;
+        const level = character.level;
+        const maxExp = await contract.call("calculateExperienceRequired", [
+          level,
+        ]);
         setCharMana(mana.toNumber());
         setCharStamina(stamina.toNumber());
+        setCharExperience(experience.toNumber());
+        setCharLevel(level.toNumber());
+        setMaxExperience(maxExp.toNumber());
       } catch (error) {
-        console.error("Error fetching mana:", error);
+        console.error("Error fetching stats:", error);
       }
     };
 
-    fetchMana();
+    fetchStats();
   }, [metadata.id, contract]);
 
   return (
@@ -50,7 +63,7 @@ const NftItem = ({ metadata, isSelected, onSelect, contract }) => {
           width={200}
           className="drop-shadow-lg"
         />
-        <div className="absolute bottom-0 left-0 mb-2 flex flex-col space-y-1 w-full px-2">
+        <div className="bottom-0 left-0 mb-2 flex flex-col space-y-1 w-full px-2">
           {charMana !== null && (
             <>
               <div className="text-[12px]">Mana</div>
@@ -63,6 +76,18 @@ const NftItem = ({ metadata, isSelected, onSelect, contract }) => {
               <ProgressBar value={charStamina} max={100} color="bg-green-700" />
             </>
           )}
+          {charExperience !== null &&
+            charLevel !== null &&
+            maxExperience !== null && (
+              <>
+                <div className="text-[12px]">Experience</div>
+                <ProgressBar
+                  value={charExperience}
+                  max={maxExperience}
+                  color="bg-yellow-500"
+                />
+              </>
+            )}
         </div>
       </div>
     </div>
