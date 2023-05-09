9ac23ecf25483412dfacfbf86e36d51a784eaa49
Kage <romzhey@gmail.com>
2023-05-09 23:06:25 +0800

    minor update

--- client/src/utils/CA.js
+++ client/src/utils/CA.js
@@ -12,3 +12,18 @@
 // export const battleContractAddress =
 //   "0xF531a467116DefF3e360d06Fd1B0D7eccEf38A9E";
 // export const battleContractABI = battleContract.abi;
+
+// contracts before 4th MAY
+// export const characterContractAddress =
+//   "0xea7912E0b0b1c1ab5220E2779eb66Da2862947C7";
+// export const characterContractABI = characterContract.abi;
+
+// export const battleSkillsAddress = "0x29922A492ccD2d07c75B485cEAeA0954E701cDE4";
+// export const battleSkillsABI = battleSkills.abi;
+
+// export const battleItemsAddress = "0xBC27613535D839f4ae9289667A2Fd21e076A0626";
+// export const battleItemsABI = battleItems.abi;
+
+// export const battleContractAddress =
+//   "0xF531a467116DefF3e360d06Fd1B0D7eccEf38A9E";
+// export const battleContractABI = battleContract.abi;
