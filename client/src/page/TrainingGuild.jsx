9ca9675df7fefca9c8d4eb813d2eda149f67cc21
Kage <romzhey@gmail.com>
2023-05-11 22:09:27 +0800

    minor update

--- client/src/page/TrainingGuild.jsx
+++ client/src/page/TrainingGuild.jsx
@@ -1,35 +1,73 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Dice } from "../utils/diceBox";
 
-import styles from '../styles';
-import { Alert } from '../components';
-import { battlegrounds } from '../assets';
-import { useGlobalContext } from '../context';
+import styles from "../styles";
+import { Alert } from "../components";
+import { battlegrounds } from "../assets";
+import { useGlobalContext } from "../context";
+
+Dice.init().then(() => {
+  document.addEventListener("mousedown", () => {
+    const diceBoxCanvas = document.getElementById("dice-canvas");
+    if (window.getComputedStyle(diceBoxCanvas).display !== "none") {
+      Dice.hide().clear();
+    }
+  });
+});
 
 const TrainingGuild = () => {
   const navigate = useNavigate();
   const { setBattleGround, setShowAlert, showAlert } = useGlobalContext();
+  const [desiredResult, setDesiredResult] = useState(1);
 
   const handleBattleChoice = (ground) => {
     setBattleGround(ground.id);
 
-    localStorage.setItem('battleground', ground.id);
+    localStorage.setItem("battleground", ground.id);
 
-    setShowAlert({ status: true, type: 'info', message: `${ground.name} is battle ready!` });
+    setShowAlert({
+      status: true,
+      type: "info",
+      message: `${ground.name} is battle ready!`,
+    });
 
     setTimeout(() => {
       navigate(-1);
     }, 1000);
   };
 
+  const handleDesiredResultChange = (e) => {
+    setDesiredResult(e.target.value);
+  };
+
+  const rollDiceWithDesiredResult = (e) => {
+    e.preventDefault();
+    Dice.show().roll("1d20", [{ set: "d20", value: parseInt(desiredResult) }]);
+    console.log("dice rolled");
+  };
+
   return (
     <div className={`${styles.flexCenter} ${styles.battlegroundContainer}`}>
-      {showAlert.status && <Alert type={showAlert.type} message={showAlert.message} />}
+      {showAlert.status && (
+        <Alert type={showAlert.type} message={showAlert.message} />
+      )}
 
-      <h1 className={`${styles.headText} text-center`}>
-        Training Guild
-        {/* <span className="text-siteViolet"> (All Battles)</span> */}
-      </h1>
+      <h1 className={`${styles.headText} text-center`}>Training Guild</h1>
+
+      <div>
+        <label htmlFor="desired-result">Desired Result (1-20): </label>
+        <input
+          id="desired-result"
+          type="number"
+          inputMode="numeric"
+          min={1}
+          max={20}
+          value={desiredResult}
+          onChange={handleDesiredResultChange}
+        />
+      </div>
+      <button onClick={rollDiceWithDesiredResult}>Roll Dice</button>
 
       <div className={`${styles.flexCenter} ${styles.battleGroundsWrapper}`}>
         {battlegrounds.map((ground) => (
@@ -38,7 +76,11 @@ const TrainingGuild = () => {
             className={`${styles.flexCenter} ${styles.battleGroundCard}`}
             onClick={() => handleBattleChoice(ground)}
           >
-            <img src={ground.image} alt="saiman" className={styles.battleGroundCardImg} />
+            <img
+              src={ground.image}
+              alt="saiman"
+              className={styles.battleGroundCardImg}
+            />
 
             <div className="info absolute">
               <p className={styles.battleGroundCardText}>{ground.name}</p>
