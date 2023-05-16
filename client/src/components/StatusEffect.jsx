b97bac7ac9c6fc11a3ddb2b8d52ada960228a3c2
Kage <romzhey@gmail.com>
2023-05-16 10:27:22 +0800

    first commit

---
+++ client/src/components/StatusEffect.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { statusEffects } from "../assets/status_effects/index.js";
+
+const StatusEffect = ({ activeEffectIds, activeEffectDurations }) => (
+  <div className="flex flex-wrap">
+    {activeEffectIds.map((effectId, index) => {
+      const effect = statusEffects.find(
+        (effect) => effect.id === effectId.toNumber()
+      );
+      if (!effect) {
+        console.warn(`Effect with ID ${effectId} not found.`);
+        return null;
+      }
+      return (
+        <div
+          key={index}
+          className="relative mr-2 mb-2 flex items-center border-2 border-gray-400 p-2 rounded-lg"
+        >
+          <img src={effect.image} alt={effect.name} className="h-4 w-4" />
+          <p className="absolute bottom-0 right-1 text-xs">
+            {activeEffectDurations[index].toNumber()}
+          </p>
+        </div>
+      );
+    })}
+  </div>
+);
+
+export default StatusEffect;
