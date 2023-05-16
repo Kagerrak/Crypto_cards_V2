bb7d1ff396e210f6949e6d7ae81843e7db392ef2
Kage <romzhey@gmail.com>
2023-05-16 10:27:15 +0800

    change some state to global

--- client/src/components/SkillSlots.jsx
+++ client/src/components/SkillSlots.jsx
@@ -1,46 +1,85 @@
-// SkillSlots.js
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import SkillSlot from "./SkillSlot";
 import { useGlobalContext } from "../context";
 
 const SkillSlots = ({ charTWContract, skillTWContract, tokenId }) => {
-  const [equippedSkills, setEquippedSkills] = useState([null, null, null]);
-
-  const { characterContract } = useGlobalContext();
+  const {
+    characterContract,
+    equippedSkills,
+    setEquippedSkills,
+    equippedSkillLoading,
+    setEquippedSkillLoading,
+    setLocalOwnedSkills,
+  } = useGlobalContext();
 
   const handleUnequipSkill = async (_skillTokenId) => {
+    setEquippedSkillLoading(true);
     try {
-      const data = await characterContract.unequipSkill(tokenId, _skillTokenId);
-      console.info("contract call successs", data);
+      const unEquiptx = await characterContract.unequipSkill(
+        tokenId,
+        _skillTokenId
+      );
+      await unEquiptx.wait();
+
+      setEquippedSkills((prevSkills) => {
+        // Step 1: Replace the unequipped skill with null
+        const newSkills = prevSkills.map((skillId) =>
+          skillId === _skillTokenId ? null : skillId
+        );
+
+        // Step 2: Rearrange the array to move null values to the end
+        const skillsWithValues = newSkills.filter(
+          (skillId) => skillId !== null
+        );
+        const nullSkills = newSkills.filter((skillId) => skillId === null);
+        return [...skillsWithValues, ...nullSkills];
+      });
+
+      // Step 3: Add the unequipped skill back to the globalOwnedSkills
+      const skillToBeAddedBack = equippedSkills.find(
+        (skill) => skill.metadata.id === _skillTokenId
+      );
+      setLocalOwnedSkills((prevSkills) => [...prevSkills, skillToBeAddedBack]);
+
+      setEquippedSkillLoading(false);
     } catch (err) {
       console.error("contract call failure", err);
     }
   };
 
   useEffect(() => {
-    if (charTWContract) {
-      const fetchEquippedSkills = async () => {
-        const fetchedSkills = await charTWContract.getEquippedSkills(tokenId);
+    const fetchEquippedSkills = async () => {
+      const fetchedSkills = await charTWContract.call("getEquippedSkills", [
+        tokenId,
+      ]);
 
-        if (fetchedSkills && fetchedSkills.length) {
-          const skillsArray = fetchedSkills.map((skillId) => {
-            return skillId !== null && skillId !== undefined
+      if (fetchedSkills) {
+        const skillsArray = new Array(3).fill(null);
+
+        fetchedSkills.forEach((skillId, index) => {
+          skillsArray[index] =
+            skillId !== null && skillId !== undefined
               ? skillId.toNumber()
               : null;
-          });
+        });
 
-          // Fill any missing skills with null to ensure the array has 3 elements
-          while (skillsArray.length < 3) {
-            skillsArray.push(null);
-          }
+        setEquippedSkills(skillsArray);
+      }
+    };
 
-          setEquippedSkills(skillsArray);
-        }
-      };
-
+    if (
+      !equippedSkillLoading &&
+      equippedSkills.every((skillId) => skillId === null)
+    ) {
       fetchEquippedSkills();
     }
-  }, [charTWContract, tokenId]);
+  }, [
+    charTWContract,
+    tokenId,
+    equippedSkills,
+    setEquippedSkills,
+    equippedSkillLoading,
+  ]);
 
   return (
     <div className="mt-4">
