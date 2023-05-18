d8a72312557668f4392bc43965174d3a987e526b
Kage <romzhey@gmail.com>
2023-05-18 23:03:07 +0800

    first commit

---
+++ client/src/assets/skills/index.js
@@ -0,0 +1,7 @@
+import Fireball from "./Fireball.png";
+import Frenzy from "./Frenzy.png";
+import Healing from "./healing_spell.png";
+import Stun from "./stun_spell.png";
+import Weaken_Attack from "./weaken_attack.png";
+import Weaken_Defense from "./weaken_defense.png";
+import Defensive_Stance from "./defensive_stance.png";
