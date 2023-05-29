8cecd050ac6d4c3f23678e18c0399d19f4b04a1a
Kage <romzhey@gmail.com>
2023-05-29 23:24:20 +0800

    initialized apollo/client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import ErrorBoundary from "./utils/ErrorBoundary";
 
 import {
@@ -25,31 +26,38 @@ import { OnboardModal, Navbar } from "./components";
 import { GlobalContextProvider } from "./context";
 import "./index.css";
 
+const client = new ApolloClient({
+  uri: "https://api.studio.thegraph.com/query/37725/wiwa/version/latest",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ErrorBoundary>
     <BrowserRouter>
       <ThirdwebProvider activeChain="mumbai">
-        <GlobalContextProvider>
-          <Navbar />
-          <OnboardModal />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/battleground" element={<Battleground />} />
-            <Route path="/battle/:battleName" element={<Battle />} />
-            <Route path="/create-battle" element={<CreateBattle />} />
-            <Route path="/join-battle" element={<JoinBattle />} />
-            <Route path="/colosseum" element={<Colosseum />} />
-            <Route path="/recruitment-guild" element={<RecruitmentGuild />} />
-            <Route path="/my-champions" element={<MyChampion />} />
-            <Route path="/training-guild" element={<TrainingGuild />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/weapon-armor-shop" element={<ItemShop />} />
-            <Route path="/item-shop" element={<WeaponShop />} />
-            <Route path="/magic-trinket-shop" element={<MagicShop />} />
-            <Route path="/inn" element={<InnPage />} />
-            <Route path="/withdraw" element={<Withdraw />} />
-          </Routes>
-        </GlobalContextProvider>
+        <ApolloProvider client={client}>
+          <GlobalContextProvider>
+            <Navbar />
+            <OnboardModal />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/battleground" element={<Battleground />} />
+              <Route path="/battle/:battleName" element={<Battle />} />
+              <Route path="/create-battle" element={<CreateBattle />} />
+              <Route path="/join-battle" element={<JoinBattle />} />
+              <Route path="/colosseum" element={<Colosseum />} />
+              <Route path="/recruitment-guild" element={<RecruitmentGuild />} />
+              <Route path="/my-champions" element={<MyChampion />} />
+              <Route path="/training-guild" element={<TrainingGuild />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/weapon-armor-shop" element={<ItemShop />} />
+              <Route path="/item-shop" element={<WeaponShop />} />
+              <Route path="/magic-trinket-shop" element={<MagicShop />} />
+              <Route path="/inn" element={<InnPage />} />
+              <Route path="/withdraw" element={<Withdraw />} />
+            </Routes>
+          </GlobalContextProvider>
+        </ApolloProvider>
       </ThirdwebProvider>
     </BrowserRouter>
   </ErrorBoundary>
