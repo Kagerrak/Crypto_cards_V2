9d7553b46c8c0a327f35b02545ce79ea3f35eaf8
Kage <romzhey@gmail.com>
2023-06-08 22:22:17 +0800

    new graph queries

--- client/src/constants/subgraphQueries.js
+++ client/src/constants/subgraphQueries.js
@@ -1,6 +1,6 @@
 import { gql } from "@apollo/client";
 
-const GET_BATTLES = gql`
+export const GET_BATTLES = gql`
   query {
     battles {
       id
@@ -12,4 +12,46 @@ const GET_BATTLES = gql`
   }
 `;
 
-export default GET_BATTLES;
+export const GET_PLAYERS = gql`
+  query {
+    players {
+      id
+      totalDamageDealt
+      totalDamageTaken
+      battles {
+        id
+        name
+        status
+        winner {
+          id
+        }
+      }
+      characters {
+        id
+        totalDamageDealt
+        totalDamageTaken
+      }
+    }
+  }
+`;
+
+export const GET_CHARACTERS = gql`
+  query {
+    characters {
+      id
+      totalDamageDealt
+      totalDamageTaken
+      owner {
+        id
+      }
+      battles {
+        id
+        name
+        status
+        winner {
+          id
+        }
+      }
+    }
+  }
+`;
