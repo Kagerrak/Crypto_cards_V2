c267a9afe45b3c0fac3e17b1b26eb40dde076ac7
Kage <romzhey@gmail.com>
2023-06-09 22:27:48 +0800

    new contract addresses

--- client/src/contract/index.js
+++ client/src/contract/index.js
@@ -4,15 +4,15 @@ import battleItems from "./battleItems.json";
 import battleContract from "./battleContract.json";
 
 export const characterContractAddress =
-  "0xB4DF2Eb5F0a429Adc600343D0BEA4A136c8f8532";
+  "0xfE9e0Ce2De8D1d1A5B4ebD43e83442f954939c32";
 export const characterContractABI = characterContract.abi;
 
-export const battleSkillsAddress = "0x54bC80Fa376BBEF04013c1F6019f79922E1bCFA7";
+export const battleSkillsAddress = "0xc545541d605CDED1a8B0c5FF861efab7046345A7";
 export const battleSkillsABI = battleSkills.abi;
 
-export const battleItemsAddress = "0xD05efF6E52C75a91A46C678624A30318Bd73f184";
+export const battleItemsAddress = "0x14E8A89289c9D3f5868152BD82d92b66301e1804";
 export const battleItemsABI = battleItems.abi;
 
 export const battleContractAddress =
-  "0x65d63958fc1Bf199e8a44762F77b329c864898fA";
+  "0x694dc36A16F733216B63aE8b91F05F76C67F8469";
 export const battleContractABI = battleContract.abi;
