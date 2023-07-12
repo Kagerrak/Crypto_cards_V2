a3833995df041bd39f1541a779077bab47d12c63
Kage <romzhey@gmail.com>
2023-07-12 09:04:09 +0800

    change text-color for readability

--- client/src/components/StatusEffect.jsx
+++ client/src/components/StatusEffect.jsx
@@ -17,7 +17,7 @@ const StatusEffect = ({ activeEffectIds, activeEffectDurations }) => (
           className="relative mr-2 mb-2 flex items-center border-2 border-gray-400 p-2 rounded-lg"
         >
           <img src={effect.image} alt={effect.name} className="h-4 w-4" />
-          <p className="absolute bottom-0 right-1 text-xs">
+          <p className="absolute bottom-0 right-1 text-xs text-gray-300">
             {activeEffectDurations[index].toNumber()}
           </p>
         </div>
