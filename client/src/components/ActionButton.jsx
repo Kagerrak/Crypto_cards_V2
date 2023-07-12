4f077fa0a0ab54faa534af796c01f87fe78a9596
Kage <romzhey@gmail.com>
2023-07-12 08:57:29 +0800

    added diabled ui

--- client/src/components/ActionButton.jsx
+++ client/src/components/ActionButton.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import styles from "../styles";
 
-const ActionButton = ({ imgUrl, handleClick, restStyles }) => (
+const ActionButton = ({ imgUrl, handleClick, restStyles, disabled }) => (
   <div
-    className={`${styles.gameMoveBox} ${styles.flexCenter} ${styles.glassEffect} ${restStyles} `}
-    onClick={handleClick}
+    className={`${styles.gameMoveBox} ${styles.flexCenter} ${
+      styles.glassEffect
+    } ${restStyles} ${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
+    onClick={!disabled ? handleClick : undefined}
   >
     <img src={imgUrl} alt="action_img" className={styles.gameMoveIcon} />
   </div>
