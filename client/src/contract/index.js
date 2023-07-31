3120c59f524b0fac66c5a7f05f0e650324e0c871
Kage <romzhey@gmail.com>
2023-07-31 09:24:06 +0800

    contract address update

--- client/src/contract/index.js
+++ client/src/contract/index.js
@@ -1,18 +1,30 @@
-import characterContract from "./characterContract.json";
-import battleSkills from "./battleSkills.json";
-import battleItems from "./battleItems.json";
-import battleContract from "./battleContract.json";
 
-export const characterContractAddress =
-  "0xc78b34142a7067f21bd3EEf6B3CbF78d938458dC";
-export const characterContractABI = characterContract.abi;
+    import characterContract from "./characterContract.json";
+    import battleSkills from "./battleSkills.json";
+    import battleItems from "./battleItems.json";
+    import battleContract from "./battleContract.json";
+    import battleEffects from "./battleEffects.json";
+    import compositeTokens from "./compositeTokens.json";
+    import equipManagement from "./equipManagement.json";
+    
+    export const characterContractAddress = "0xcDA9aD92E91eC025F17BFD178138d34b72C892e6";
+    export const characterContractABI = characterContract.abi;
+    
+    export const battleSkillsAddress = "0x6f30952f43e1C7369bB49D2D0739B5df4c72FA52";
+    export const battleSkillsABI = battleSkills.abi;
+    
+    export const battleItemsAddress = "0x8eA90C22016DDE0DAb85d78a05604C4D1B6b2631";
+    export const battleItemsABI = battleItems.abi;
+    
+    export const battleContractAddress = "0xdE4707B1962E71cB8F5DCd8bD9F4D1D1613Fd973";
+    export const battleContractABI = battleContract.abi;
+    
+    export const battleEffectsAddress = "0x29d45F6f8Bb8CabC4472D0bc9D85f273Cc56e691";
+    export const battleEffectsABI = battleEffects.abi;
+    
+    export const compositeTokensAddress = "0x8b9e0CFfD898E33659CDc8f0ABfb6c74539f9E36";
+    export const compositeTokensABI = compositeTokens.abi;
 
-export const battleSkillsAddress = "0xbA1fb670D26EedbE0988e7c7b5b18701b08c8C75";
-export const battleSkillsABI = battleSkills.abi;
-
-export const battleItemsAddress = "0x403B63A6708d0E936EEe874D62b97497c03c2d5F";
-export const battleItemsABI = battleItems.abi;
-
-export const battleContractAddress =
-  "0x7DE5c693E3f9e2Be2baCfebA5FE5d0A7b8cAb129";
-export const battleContractABI = battleContract.abi;
+    export const equipManagementAddress = "0xbB5072F4C8C2a3ee3ca297B0b26497AF05D357d7";
+    export const equipManagementABI = equipManagement.abi;
+  
