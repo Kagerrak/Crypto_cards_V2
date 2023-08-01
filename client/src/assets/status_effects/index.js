38d2a4a7c9ddb753d413cf9ee197c8500633f216
Kage <romzhey@gmail.com>
2023-08-01 09:20:05 +0800

    adjusted to handle composite ids

--- client/src/assets/status_effects/index.js
+++ client/src/assets/status_effects/index.js
@@ -10,43 +10,43 @@ import boostDefense from "./boost_defense.png";
 // Creating an array of objects where each object contains the status effect ID, name, description and image
 export const statusEffects = [
   {
-    id: 0,
+    id: 1,
     name: "Stun",
     description: "Miss a turn",
     image: stun,
   },
   {
-    id: 1,
+    id: 2,
     name: "Damage Over Time",
     description: "Take damage each turn",
     image: burn,
   },
   {
-    id: 2,
+    id: 3,
     name: "Reduce Attack",
     description: "Reduces attack power",
     image: reduceAttack,
   },
   {
-    id: 3,
+    id: 4,
     name: "Reduce Defense",
     description: "Reduces defense power",
     image: reduceDefense,
   },
   {
-    id: 4,
+    id: 5,
     name: "Boost Attack",
     description: "Increases attack power",
     image: boostAttack,
   },
   {
-    id: 5,
+    id: 6,
     name: "Heal Over Time",
     description: "Regain health each turn",
     image: regen,
   },
   {
-    id: 6,
+    id: 7,
     name: "Defense Boost",
     description: "Increases defense power",
     image: boostDefense,
