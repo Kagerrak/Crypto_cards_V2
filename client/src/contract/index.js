de1ffd618e0555a869913270c3264b9fc6af5446
Kage <romzhey@gmail.com>
2023-08-02 10:02:28 +0800

    contract updates

--- client/src/contract/index.js
+++ client/src/contract/index.js
@@ -7,27 +7,27 @@ import compositeTokens from "./compositeTokens.json";
 import equipManagement from "./equipManagement.json";
 
 export const characterContractAddress =
-  "0x89Fdb9736A10085c07F3912b76c520533567a62d";
+  "0xA9B86e27e91255eeFc409C0eA0408c5dE8720106";
 export const characterContractABI = characterContract.abi;
 
-export const battleSkillsAddress = "0xeb8AC77F5217045B4D73d7D81E4EF586C72AF5F6";
+export const battleSkillsAddress = "0xc9E6F8EF48cb8C5079879cC0470DBD88E53dc511";
 export const battleSkillsABI = battleSkills.abi;
 
-export const battleItemsAddress = "0x1A74a7C2500d571f68D9FD79d752Fc6bdA7B33E4";
+export const battleItemsAddress = "0x82fC14f361CC77C16998427F1895c22bb2293bBc";
 export const battleItemsABI = battleItems.abi;
 
 export const battleContractAddress =
-  "0xE74001D799A748bf9EB4572bb3e57163Fae26701";
+  "0xb0889c119FD81D70aE104f1bB29C9167B89CBC08";
 export const battleContractABI = battleContract.abi;
 
 export const battleEffectsAddress =
-  "0x5232542657067c1087C936e254F05525f2f366D0";
+  "0x684a84d5C5fA95318d7617c4C87D40C46DBF8b2a";
 export const battleEffectsABI = battleEffects.abi;
 
 export const compositeTokensAddress =
-  "0xf8F19AAbb6Cdaf7aa09c02d31e29D500CFD0D491";
+  "0xf377AaCebcfF6248EE481Fe22308Ae490aB199A4";
 export const compositeTokensABI = compositeTokens.abi;
 
 export const equipManagementAddress =
-  "0xdC850Ca3d7d7aCCC58226834557151dB631E67b1";
+  "0xA14284E607ced64a83ec3907ff27E19bD644edDC";
 export const equipManagementABI = equipManagement.abi;
