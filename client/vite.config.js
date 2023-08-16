df45f950c4078d3d0f891a7753c0a45658623762
Kage <romzhey@gmail.com>
2023-08-16 23:55:38 +0800

    changed vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,20 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dns from "dns";
+
+dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     "process.env": {},
+  },
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
   },
 });
